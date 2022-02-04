eb24437e266631b1937154c51edb52cdbde2234b
Aurelian F <85421377+aurefousse@users.noreply.github.com>
2022-02-03 18:52:54 -0800

    Addding homework

---
+++ Level-1/SQL_File.sql
@@ -0,0 +1,7 @@
+CREATE TABLE review_id_table (
+  review_id TEXT PRIMARY KEY NOT NULL,
+  customer_id INTEGER,
+  product_id TEXT,
+  product_parent INTEGER,
+  review_date DATE -- this should be in the formate yyyy-mm-dd
+);
